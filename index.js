dd89cd3e82977f0ca3b25995e6d1c9ff32aca833
codeninja <adarsha.pau@gmail.com>
2024-12-24 14:27:10 +0545

    done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const cron = require("node-cron");
 // Middleware for parsing JSON request bodies
 app.use(express.json());
 // Import routes
@@ -10,6 +12,19 @@ const { default: axios } = require("axios");
 // Mount routes
 app.use("/api/items", getItems);
 app.use("/api/post", postItems);
+const url = process.env.MYURL;
+cron.schedule("*/1 * * * *", async () => {
+  try {
+    console.log("Automated blog post creation started...");
+    const response = await axios.get(`${url}/api/post`);
+    console.log("Blog post created successfully", response.data);
+  } catch (error) {
+    console.error(
+      "Error while creating blog post automatically:",
+      error.message
+    );
+  }
+});
 
 // Start the server
 app.listen(port, () => {
